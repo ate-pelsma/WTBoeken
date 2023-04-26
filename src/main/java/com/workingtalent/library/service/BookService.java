53d509993b92362dc094a3098b624697be26d8ba
Ate Pelsma <87018841+ate-pelsma@users.noreply.github.com>
2023-04-26 11:28:07 +0200

    book layers created

--- src/main/java/com/workingtalent/library/service/BookService.java
+++ src/main/java/com/workingtalent/library/service/BookService.java
@@ -1,4 +1,16 @@
 package com.workingtalent.library.service;
 
+import com.workingtalent.library.repository.IBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+
+    @Autowired
+    private IBookRepository bookRepo;
+
+    public void createBook(){
+        System.out.printf("We zitten in boek service");
+    }
 }
