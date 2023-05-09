da0fb8808cb5b164b5a15ce0c20b90589536dd45
noahdg <noah.d.g@hotmail.com>
2023-05-09 16:37:51 +0200

    user setup voor frontend

--- src/main/java/com/workingtalent/library/entities/User.java
+++ src/main/java/com/workingtalent/library/entities/User.java
@@ -22,7 +22,7 @@ public class User {
 	@Column(length = 100, nullable = false)
 	private String name;
 	
-	@Column(length = 50, nullable = false)
+	@Column(length = 100, nullable = false)
 	private String password;
 	
 	@Column(length = 100, nullable = false, unique = true)
