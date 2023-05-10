e3e736156d2598e9b90fbdbbe9781ce2c714d3b6
Ate Pelsma <87018841+ate-pelsma@users.noreply.github.com>
2023-05-10 10:15:37 +0200

    security config weer toegevoegd

---
+++ src/main/java/com/workingtalent/library/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.workingtalent.library.config;
+
+import com.workingtalent.library.service.JpaUserDetailsService;
+import com.workingtalent.library.utils.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig{
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Autowired
+    public JpaUserDetailsService jpaUserDetailsService;
+
+    @Bean
+    public AuthenticationManager authManager(JpaUserDetailsService jpaUserDetailsService)
+    {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(jpaUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(authProvider);
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests( auth -> auth
+                        //Everyone can visit the start page ("/"), all other pages require a log in.
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                //Load login request
+                .headers(headers -> headers.frameOptions())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .cors(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer{
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
+        }
+    }
+}
