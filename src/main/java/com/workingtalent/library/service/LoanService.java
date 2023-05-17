7bb8c908b2554c4c3ecf788c05c5d5b920542de1
noahdg <noah.d.g@hotmail.com>
2023-05-17 17:22:50 +0200

    loandto

--- src/main/java/com/workingtalent/library/service/LoanService.java
+++ src/main/java/com/workingtalent/library/service/LoanService.java
@@ -1,14 +1,20 @@
 package com.workingtalent.library.service;
 
+import com.workingtalent.library.dto.LoanDto;
+import com.workingtalent.library.entities.Book;
 import com.workingtalent.library.entities.Copy;
 import com.workingtalent.library.entities.Loan;
+import com.workingtalent.library.entities.Reservation;
+import com.workingtalent.library.entities.ReservationStatus;
 import com.workingtalent.library.entities.User;
 import com.workingtalent.library.repository.ICopyRepository;
 import com.workingtalent.library.repository.ILoanRepository;
+import com.workingtalent.library.repository.IReservationRepository;
 import com.workingtalent.library.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,22 +28,30 @@ public class LoanService {
 	
 	@Autowired
 	private ICopyRepository copyRepo;
-
-	public void saveLoan(Loan loan, long userid, long copyid){
-		Copy copy = copyRepo.findById(copyid).get();
+	
+	@Autowired
+	private IReservationRepository resRepo;
+	
+	public void saveLoanFromReservation(long reservationid, long copyid) {
+		Reservation reservation = resRepo.findById(reservationid).orElseThrow(() -> new IllegalArgumentException("Reservation not found"));
+		Copy copy = copyRepo.findById(copyid).orElseThrow(() -> new IllegalArgumentException("Copy not found"));
 		List<Loan> copyLoans = copy.getLoans();
+		User user = reservation.getUser();
+		List<Loan> userLoans = user.getLoans();
+		
+		Loan loan = new Loan(copy, user);
+		
 		copyLoans.add(loan);
 		copy.setLoans(copyLoans);
+		copy.setLoaned(true);
 		copyRepo.save(copy);
 
-		User user = userRepo.findById(userid).get();
-		List<Loan> userLoans = user.getLoans();
 		userLoans.add(loan);
 		user.setLoans(userLoans);
 		userRepo.save(user);
-
-		loan.setCopy(copy);
-		loan.setUser(user);
+		
+		reservation.setStatus(ReservationStatus.ACCEPTED);
+		
 		loanRepo.save(loan);
 	}
 
@@ -62,4 +76,36 @@ public class LoanService {
 
 		loanRepo.save(loan);
 	}
+	
+	public LoanDto findLoan(long id) {
+		Loan loan = loanRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("Loan not found"));
+		return convertToDto(loan);
+	}
+	
+	public List<LoanDto> findAllFromUser(long userid) {
+		User user = userRepo.findById(userid).orElseThrow(() -> new IllegalArgumentException("User not found"));
+		List<Loan> loans = user.getLoans();
+		List<LoanDto> loanDtos = new ArrayList<>();
+		for (Loan loan : loans) {
+			loanDtos.add(convertToDto(loan));
+		}
+		return loanDtos;
+	}
+	
+	private LoanDto convertToDto(Loan loan) {
+		LoanDto loanDto = new LoanDto();
+		Book book = loan.getCopy().getBook();
+		loanDto.setId(loan.getId());
+		loanDto.setStartDate(loan.getStartDate());
+		loanDto.setEndDate(loan.getEndDate());
+		loanDto.setUserid(loan.getUser().getId());
+		loanDto.setUserName(loan.getUser().getName());
+		loanDto.setCopyid(loan.getCopy().getId());
+		loanDto.setBookid(book.getId());
+		loanDto.setBookTitle(book.getTitle());
+		loanDto.setBookAuthor(book.getAuthor());
+		loanDto.setBookIsbn(book.getIsbn());
+		return loanDto;
+	}
+
 }
